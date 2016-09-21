ccd0f61d8fa9c05a5c69448d8297faae33f3e38a
deryagin <deryagin.and@gmail.com>
2016-09-22 00:28:03 +0300

    New. Morgan request logger added.

--- src/config/express.conf.js
+++ src/config/express.conf.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const requestLogger = require('../config/morgan.conf');
 module.exports.configure = configure;
 
 function configure(app) {
   app.use(bodyParser.json());
   app.use(compression());
+  app.use(requestLogger);
 }
---
+++ src/config/morgan.conf.js
@@ -0,0 +1,6 @@
+const morgan = require('morgan');
+module.exports = configure();
+
+function configure() {
+  return morgan('[:date[iso]] :remote-addr :method :url :status :res[content-length] :response-time ms');
+}
