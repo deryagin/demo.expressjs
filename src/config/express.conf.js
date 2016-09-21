0eb58cf31ea416dda5e37f7d54555e7a700594e3
deryagin <deryagin.and@gmail.com>
2016-09-22 00:20:30 +0300

    New. Express compression middleware added.

--- src/config/express.conf.js
+++ src/config/express.conf.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
+const compression = require('compression');
 module.exports.configure = configure;
 
 function configure(app) {
   app.use(bodyParser.json());
+  app.use(compression());
 }
