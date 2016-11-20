3f475e2a883c7f6d469ee0c29ef30337a8e16629
deryagin <deryagin.and@gmail.com>
2016-11-20 14:34:46 +0300

    Enh. Error config refactored.

--- srv/config/errors.conf.js
+++ srv/config/errors.conf.js
@@ -4,19 +4,22 @@ module.exports.configure = configure;
 function configure(app) {
   app.use(handle4xx);
   app.use(handle5xx);
+  handleUncought();
 }
 
 function handle4xx(err, req, res, next) {
-  console.error('Unhandled 4xx User Error', err.stack);
-  let reply = () => res.status(400).json({'error': err.message});
-  return (error instanceof ApiError ? reply() : next(err));
+  console.error('400 Bad Request', err.stack);
+  let reply400 = () => res.status(400).json({'error': err.message});
+  return (error instanceof ApiError ? reply400() : next(err));
 }
 
 function handle5xx(err, req, res, next) {
-  console.error('Internal Server Error', err.stack);
-  let reply = () => res.status(500).json({'error': 'Internal Server Error'});
-  return (err ? reply() : next(err));
+  console.error('500 Internal Server Error', err.stack);
+  let reply500 = () => res.status(500).json({'error': 'Internal Server Error'});
+  return (err ? reply500() : next(err));
 }
 
-process.on('uncaughtException', console.error);
+function handleUncought() {
+  process.on('uncaughtException', console.error);
+}
 
