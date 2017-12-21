03d5bf77abb36be1e40d13f82a0a6382af1604c4
deryagin <deryagin.and@gmail.com>
2017-12-21 14:56:12 +0300

    Enh. Eslint airbnb config replaced by airbnb-base.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@
 // ./node_modules/.bin/eslint --print-config srv | less
 
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": ["airbnb"],
-    "rules": {
-        "indent": ["error", 2, { "SwitchCase": 1 }],
-        "max-len": ["error", { "ignoreUrls": true, "code": 120 }],
-        "yoda": ["error", "always", { "exceptRange": true, "onlyEquality": true }],
-        "object-curly-spacing": ["error", "never"],
-        "arrow-parens": ["off"],
-        "consistent-return": ["off"],
-        "no-plusplus": ["off"],
-        "no-shadow": ["off"],
-        "no-use-before-define": ["off"],
-        "object-shorthand": ["off"],
-        "prefer-const": ["off"],
-    }
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': ['airbnb-base'],
+  'rules': {
+    'arrow-parens': ['off'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'consistent-return': ['off'],
+    'indent': ['error', 2, {'SwitchCase': 1}],
+    'max-len': ['error', {'ignoreUrls': true, 'code': 120}],
+    'no-shadow': ['off'],
+    'no-use-before-define': ['off'],
+    'object-curly-spacing': ['error', 'never'],
+    'object-shorthand': ['off'],
+    'prefer-const': ['off'],
+    'yoda': ['error', 'always', {'exceptRange': true, 'onlyEquality': true}],
+  }
 };
