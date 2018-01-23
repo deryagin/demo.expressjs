d77fdadc7786ff5f71d08398e75b7cf53a41d0e2
deryagin <deryagin.and@gmail.com>
2018-01-23 14:03:39 +0300

    Enh. ApiError inheritance setup.

--- srv/utility/ApiError.js
+++ srv/utility/ApiError.js
@@ -1,10 +1,20 @@
 module.exports = ApiError;
 
 function ApiError(message) {
-  Error.call(this);
-  this.name = this.constructor.name;
   this.message = message;
-  this.stack = (new Error()).stack;
+  Error.captureStackTrace(this, ApiError);
 }
 
 ApiError.prototype = Object.create(Error.prototype);
+ApiError.prototype.name = ApiError.name;
+ApiError.prototype.constructor = ApiError;
+
+// console.log(ApiError);
+// console.log(ApiError.prototype);
+// console.log(ApiError.prototype.constructor);
+// console.log(ApiError.prototype.constructor.name);
+// console.log(ApiError.prototype.__proto__);
+// console.log(new ApiError('asdf') instanceof Error);
+// console.log(new ApiError('asdf') instanceof ApiError);
+// console.log(new ApiError('asdf').stack);
+// console.log(new ApiError('asdf').message);
